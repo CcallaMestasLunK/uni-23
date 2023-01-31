ce23ffa21a22404154106295908a717227ba636d
karina <20182001tiempo@gmail.com>
2023-01-30 19:45:53 -0500

    30 -01

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -1,17 +1,23 @@
 <?php
 
-	class ControladorUsuarios{
+	class ControladorUsuarios
+    {
 
-                static public function ctrCrearUsuario(){
+                static public function ctrCrearUsuario()
+                {
                     if(isset($_POST['nuevoUsuario'])){
-                    if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST['nuevoNombre'])&&
-                    preg_match('/^[a-zA-Z0-9]+$/', $_POST['nuevoUsuario'])&&
-                    preg_match('/^[a-zA-Z0-9]+$/', $_POST['nuevoPassword'])){
+                    if(
+                        preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST['nuevoNombre'])&&
+                        preg_match('/^[a-zA-Z0-9]+$/', $_POST['nuevoUsuario'])&&
+                        preg_match('/^[a-zA-Z0-9]+$/', $_POST['nuevoPassword'])
+                    ) {
 
-                        $ruta="";
-                        if(isset($_FILES['nuevaFoto']['tmp_name'])){
+                        $ruta= "";
+                        if(isset($_FILES['nuevaFoto']['tmp_name'])) {
 
-                                list($ancho, $alto) = getimagesize($_FILES['nuevaFoto']['tmp_name']);
+                            var_dump($_FILES['nuevaFoto']['tmp_name']);
+                            exit;
+                            list($ancho, $alto) = getimagesize($_FILES['nuevaFoto']['tmp_name']);
                                 $nuevoancho = 500;
                                 $nuevoalto = 500;
                     
@@ -22,14 +28,14 @@
 
                                 // proceso de recorte de la foto
 
-                                if($_FILES['nuevaFoto']['type']=="image/jpg"){
+                                if($_FILES['nuevaFoto']['type']=="image/jpeg"){
 
-                                    $aleatorio = mt_rand(100,999);
-                                    $ruta = $directorio."/".$aleatorio.".jpg";
-                                    $origen = imagecreatefromjpg($_FILES['nuevaFoto']['tmp_name']);
+                                    $aleatorio = mt_rand(100, 999);
+                                    $ruta = $directorio. "/" .$aleatorio . " .jpg";
+                                    $origen = imagecreatefromjpeg($_FILES['nuevaFoto']['tmp_name']);
                                     $destino = imagecreatetruecolor($nuevoancho, $nuevoalto);
                                     imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoancho, $nuevoalto, $ancho, $alto);
-                                    imagejpg($destino,$ruta);
+                                    imagejpeg($destino,$ruta);
                                 }
                                 if($_FILES['nuevaFoto']['type']=="image/png"){
 
@@ -47,26 +53,23 @@
                                         "usuario" => $_POST['nuevoUsuario'],
                                         "password" => $_POST['nuevoPassword'],
                                         "perfil" => $_POST['nuevoPerfil'],
-                                        "ruta" => $_POST['ruta']);
+                                        "ruta" => $ruta
+                        );
                         $respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla,$datos);
-                        if($respuesta =="ok"){
+                        if($respuesta == "ok"){
 
                             echo ("<script>
 
-                            Swal.fire({
-                                        title: 'Success!',
-                                        text: '¡Registro Exitoso!',
-                                        icon: 'success',
-                                        confirmButtonText: 'Ok'
-                                    
-                                    });
+                                        Swal.fire({
+                                                    title: 'Success!',
+                                                    text: '¡Registro Exitoso!',
+                                                    icon: 'success',
+                                                    confirmButtonText: 'Ok'
+                                                
+                                                });
 
-                        </script>");
-
-                        }
-
-                    
-
+                                    </script>");
+                        }                   
                     }else{
                         //echo ('<script>alert ("ingreso");</script>');
                         echo ("<script>
@@ -84,21 +87,22 @@
             }               
     
 
-    static public function ctrIngreso(){
+    static public function ctrIngreso()
+    {
             if(isset($_POST['ingUsuario'])){
                 if(preg_match('/^[a-zA-Z0-9]+$/', $_POST['ingUsuario'])&&preg_match('/^[a-zA-Z0-9]+$/', $_POST['ingPassword'])){
                     $tabla = "usuarios";
                     $item = "usuario";
                     $valor = $_POST['ingUsuario'];
                     $salt = md5($_POST['ingPassword']);
-                    $passwordEncriptado = crypt($_POST['ingPassword'],$salt);
+                    $passwordEncriptado = $_POST['ingPassword'];
                     $respuesta = ModeloUsuarios::mdlMostrarUsuarios($tabla,$item,$valor);
                     if($respuesta['usuario']==$_POST['ingUsuario']&&$respuesta['password']==$passwordEncriptado){
-                                if($respuesta['estado']==1){
-                                $_SESSION['iniciarSesion']="ok";
+                                if($respuesta['estado'] == 1) {
+                                $_SESSION['iniciarSesion'] = "ok";
                                 echo '<script>
-                                    window.location = "inicio";
-                                </script>';
+                                       window.location = "inicio";
+                                        </script>';
                                                             
                     }else{
                         echo("<div class='alert alert-danger'>Error al ingresar al sistema</div>");
